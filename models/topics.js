aa78fbf80b809faa5e9f370f14440c23a7e2bf21
jayascript1 <jojlondon@gmail.com>
2023-03-28 14:53:11 +0100

    add Poonam's changes

--- models/topics.js
+++ models/topics.js
@@ -3,11 +3,6 @@ const db = require("../db/connection");
 exports.fetchTopics = () => {
   return db.query("SELECT * FROM topics").then((result) => {
     const topics = result.rows;
-    if (topics.length === 0) {
-      const error = new Error("Topics not found");
-      error.status = 404;
-      throw error;
-    }
     return topics;
   });
 };
