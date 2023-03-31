b39f267feff629fb857a6866a60c2804302412bb
jayascript1 <jojlondon@gmail.com>
2023-03-31 15:13:18 +0100

    add fucntionality got GET /api to respond with JSON describing all the available endpoints on the API

--- app.js
+++ app.js
@@ -27,6 +27,60 @@ app.delete("/api/comments/:comment_id", deleteCommentById);
 
 app.get("/api/users", getUsers);
 
+app.get('/api', (req, res) => {
+  const endpoints = {
+    "GET /api": {
+      "description": "serves up a json representation of all the available endpoints of the api"
+    },
+    "GET /api/topics": {
+      "description": "serves an array of all topics",
+      "queries": [],
+      "exampleResponse": {
+        "topics": [{ "slug": "football", "description": "Footie!" }]
+      }
+    },
+    "GET /api/articles": {
+      "description": "serves an array of all articles",
+      "queries": ["author", "topic", "sort_by", "order"],
+      "exampleResponse": {
+        "articles": [
+          {
+            "title": "Seafood substitutions are increasing",
+            "topic": "cooking",
+            "author": "weegembump",
+            "body": "Text from the article..",
+            "created_at": 1527695953341
+          }
+        ]
+      }
+    },
+    "GET /api/articles/:article_id": {
+      "description": "returns an article by article_id"
+    },
+    "GET /api/articles/:article_id/comments": {
+      "description": "returns all comments for an article by article_id"
+    },
+    "GET /api/topics": {
+      "description": "returns all topics"
+    },
+    "GET /api/topics/:topic/articles": {
+      "description": "returns all articles for a specific topic"
+    },
+    "GET /api/users": {
+      "description": "returns all users"
+    },
+    "GET /api/users/:username": {
+      "description": "returns a user by username"
+    },
+    "DELETE /api/comments/:comment_id": {
+      "description": "deletes a comment by comment_id"
+    }
+  };
+
+  res.json(endpoints);
+});
+
+
 app.all("/*", (req, res) => {
   res.status(404).send({ message: "Not Found" });
 });
@@ -37,7 +91,7 @@ app.use((err, req, res, next) => {
     res.status(400).send({ message: "Bad request" });
   } else if (err.status === 404) {
     res.status(404).send({ message: "Not found" });
-  } else if (err.code === "23502" && err.column === "author") {
+  } else if (err.code === "23502" && err.column === "author") {g
     res.status(400).send({ message: "Username not provided" });
   } else if (err.code === "23502" && err.column === "body") {
     res.status(400).send({ message: "Comment not provided" });
--- controllers/postComment.js
+++ controllers/postComment.js
@@ -1,8 +1,14 @@
 const { insertComment } = require("../models/insertComment");
 
 exports.postComment = (req, res, next) => {
+  
   const { username, body, ...extra } = req.body;
   const { article_id } = req.params;
+
+  if (!username) {
+    return res.status(400).json({ message: "Username not provided" });
+  }
+
   insertComment({ username, body }, article_id)
     .then((comment) => {
       res.status(201).send({ comment });
