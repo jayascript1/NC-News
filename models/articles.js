f77de306af6c02af6b74a46be15709ad276025b4
jayascript1 <jojlondon@gmail.com>
2023-03-31 14:14:42 +0100

    add functionality to query articles

--- models/articles.js
+++ models/articles.js
@@ -1,23 +1,22 @@
 const db = require("../db/connection");
 
-exports.fetchArticles = () => {
-  return db
-    .query(
-      `
-    SELECT articles.article_id, articles.title, articles.body, articles.votes, articles.topic, articles.author, articles.created_at, articles.article_img_url, COUNT(comments.comment_id) AS comment_count
+exports.fetchArticles = ({ topic, sort_by = "created_at", order = "desc" } = {}) => {
+  let query = `
+    SELECT articles.article_id, articles.title, articles.body, articles.votes, articles.topic, articles.author, articles.created_at, articles.article_img_url
     FROM articles
-    LEFT JOIN comments ON articles.article_id = comments.article_id
-    GROUP BY articles.article_id
-    ORDER BY articles.created_at DESC
-  `
-    )
-    .then((result) => {
-      const articles = result.rows.map((article) => {
-        return {
-          ...article,
-          comment_count: parseInt(article.comment_count),
-        };
-      });
-      return articles;
-    });
+    `;
+  const params = [];
+
+  if (topic) {
+    query += `WHERE articles.topic = $1 `;
+    params.push(topic);
+  }
+
+  query += `
+    ORDER BY ${sort_by} ${order}
+  `;
+
+  return db.query(query, params).then((result) => {
+    return result.rows;
+  });
 };
